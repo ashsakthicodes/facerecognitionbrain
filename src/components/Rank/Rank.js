9f9a976cd569bace962ca2dd62151b2045711dbe
ashwini sakthivel <ashsakthi.code@gmail.com>
2020-10-25 01:09:28 -0400

    Components Added

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Rank = () => {
+	return (
+		<div>
+			<div className='white f3'>
+				{'Ashwini, your current is...'}
+			</div>
+			<div className='white f1'>
+				{'#5'}
+			</div>
+		</div>
+	);
+}
+export default Rank;
